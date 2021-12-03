ee2359ad7c25ef4d7a58c58bc2de8a7023a2d569
Wang Junwei <vslegend@foxmail.com>
2021-12-03 17:22:52 +0800

    BlockingQueue测试

---
+++ src/main/java/com/training/thread/atomic/AtomicTest.java
@@ -0,0 +1,41 @@
+package com.training.thread.atomic;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Author Wong Jwei
+ * @Date 2021/12/3
+ * @Description
+ */
+public class AtomicTest {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        final AtomicInteger count = new AtomicInteger();
+        final int capacity = 100;
+        // put
+        Thread put = new Thread(() -> {
+            while (count.get() <= capacity && !Thread.currentThread().isInterrupted()) {
+                int increment = count.getAndIncrement();
+                System.out.println("PUT:" + increment);
+            }
+        });
+        // take
+        Thread take = new Thread(() -> {
+            while (count.get() > 0  && !Thread.currentThread().isInterrupted()) {
+                int decrement = count.getAndDecrement();
+                System.out.println("TAKE:" + decrement);
+            }
+        });
+
+
+
+        put.start();
+        take.start();
+        Thread.sleep(5000);
+        put.interrupt();
+        take.interrupt();
+    }
+
+}
--- src/main/java/com/training/thread/concurrentCollection/BlockingQueueTest.java
+++ src/main/java/com/training/thread/concurrentCollection/BlockingQueueTest.java
@@ -2,9 +2,6 @@ package com.training.thread.concurrentCollection;
 
 import java.util.Random;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.LockSupport;
 
 /**
  * @User: Wang Junwei
@@ -14,44 +11,44 @@ import java.util.concurrent.locks.LockSupport;
 public class BlockingQueueTest {
 
     public static void main(String[] args) {
-        final LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
-
+        final BlockingQueue<String> queue = new LinkedBlockingQueue<>(1);
         // 生产者，
         new Thread(() -> {
             try {
-                int time = new Random().nextInt(1000);
+                int time = new Random().nextInt(200);
+                System.out.println("生产者休眠" + time + "");
                 Thread.sleep(time);
-                System.out.println(Thread.currentThread().getName() + " 开始生产");
-                queue.put(1);
-                System.out.println(Thread.currentThread().getName() + " 生产");
-                queue.put(2);
-                System.out.println(Thread.currentThread().getName() + " 生产");
+                System.out.println("生产者开始生产");
+                queue.put("商品1");
+                System.out.println("生产者生产1");
+                queue.put("商品2");
+                System.out.println("生产者生产2");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }).start();
 
-        // 忠实客户，一直等，直到有货
+        // 忠实消费者，一直等，直到有货
         new Thread(() -> {
             try {
-                Integer peek = queue.peek();
-                System.out.println(Thread.currentThread().getName() + " 查看：" + peek);
-                Integer take = queue.take();
-                System.out.println(Thread.currentThread().getName() + " 消费：" + take);
+                String peek = queue.peek();
+                System.out.println("忠实消费者查看有无货：" + peek);
+                String take = queue.take();
+                System.out.println("忠实消费者消费：" + take);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }).start();
 
-        // 普通客户，指定时间内每货，就不需要了
+        // 普通消费者，指定时间内每货，就不需要了
         new Thread(() -> {
             try {
-                int time = new Random().nextInt(1000);
-                Integer poll = queue.poll(time, TimeUnit.MILLISECONDS);
+                int time = new Random().nextInt(100);
+                String poll = queue.poll(time, TimeUnit.MILLISECONDS);
                 if (null != poll) {
-                    System.out.println(Thread.currentThread().getName() + " 限定时间" + time + "内消费：" + poll);
+                    System.out.println("普通消费者 限定时间" + time + "内消费：" + poll);
                 } else {
-                    System.out.println(Thread.currentThread().getName() + " 限定时间" + time + "内未能消费");
+                    System.out.println("普通消费者 限定时间" + time + "内未能消费");
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
